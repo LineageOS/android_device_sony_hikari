a225e728a1028b21911a779a3908fe9da7bee7e3
Giulio Cervera <giulio.cervera@gmail.com>
2012-08-17 10:19:18 +0200

    hikari: update defconfig name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@ USE_CAMERA_STUB := true
 -include device/sony/fuji-common/BoardConfigCommon.mk
 
 TARGET_KERNEL_SOURCE := kernel/sony/msm8660
-TARGET_KERNEL_CONFIG := cyanogen_hikari_defconfig
+TARGET_KERNEL_CONFIG := cyanogen_hikari_row_defconfig
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 16
