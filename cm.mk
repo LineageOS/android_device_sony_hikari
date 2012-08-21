5f526a4d5130dc97feba181401783b947418dc94
Giulio Cervera <giulio.cervera@gmail.com>
2012-08-21 21:57:05 +0200

    hikari: remove bootanimation settings
    moved to fuji-common as most (if not all) fuji have a 1280x720 panel
    
    Change-Id: Ia7dd5f5b724a60e44485f4e79702e86f07a6993e

--- cm.mk
+++ cm.mk
@@ -11,7 +11,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26w_1266-3189 BUILD_FINGERPRINT=SEMC/LT26w_1266-3189/LT26w:4.0.4/6.1.A.1.58/xft_zw:user/release-keys PRIVATE_BUILD_DESC="LT26w-user 4.0.4 6.1.A.1.58 xft_zw test-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-720x1280
-
 PRODUCT_NAME := cm_hikari
 PRODUCT_DEVICE := hikari
